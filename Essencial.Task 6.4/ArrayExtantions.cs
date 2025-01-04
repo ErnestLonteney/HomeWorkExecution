218f74923f2e20ce6cd9bdce800ce120b99d8b54
Volodymyr <Volodymyr@gmail.com>
2025-01-04 13:37:29 +0100

    Added extesion

---
+++ Essencial.Task 6.4/ArrayExtantions.cs
@@ -0,0 +1,21 @@
+﻿namespace Essencial.Task_6._4
+{
+    static class ArrayExtantions
+    {
+        public static void BubbleSort(this int[] array)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = 0; j < array.Length - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        var buf = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = buf; 
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Essencial.Task 6.4/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Essencial.Task_6._4
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int[] array = [23, 45, 12, 23, 78, 0];
+
+            array.BubbleSort(); 
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);    
+            }
+        }
+    }
+}
