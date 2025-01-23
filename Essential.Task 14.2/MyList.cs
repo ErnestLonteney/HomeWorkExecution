ed425fef844b9c5816db6bf113a3d6ed6c07c75e
Volodymyr <Volodymyr@gmail.com>
2025-01-23 18:25:22 +0100

    Added task 14.2

---
+++ Essential.Task 14.2/MyList.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+
+namespace Essential.Task_14._2
+{
+    class MyList<T> : IEnumerable<T>, IEnumerator<T>    
+    {
+        private T[] elements;
+        private int currentPossition = 0;
+        private int position = -1;
+
+        public MyList()
+        {
+            elements = new T[10];
+        }
+
+        public MyList(IEnumerable<T> elements)
+        {
+            this.elements = new T[elements.Count() * 2];
+
+            int i = 0;
+            foreach (T element in elements)
+            {
+                this.elements[i++] = element;
+            }
+
+            currentPossition = elements.Count();
+        }
+
+        public void Add(T newElement)
+        {
+            if (currentPossition == elements.Length / 2)
+            {
+                ResizeInnerArray();             
+            }
+
+            elements[currentPossition++] = newElement;
+        }
+
+        public int Count => elements.Length;
+
+        private void ResizeInnerArray()
+        {
+            var newArray = new T[elements.Length * 2];
+            Array.Copy(elements, newArray, elements.Length);
+            this.elements = newArray;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return this;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return (IEnumerator)this;
+        }
+
+        public void Reset() => position = -1;
+
+        public T Current => elements[position];
+
+        object IEnumerator.Current => elements[position];
+
+        public bool MoveNext()
+        {
+            if (position != currentPossition - 1)
+            {
+                position++; 
+                return true;
+            }
+
+            return false;
+        }
+
+        public void Dispose()
+        {
+            Reset();
+        }
+
+        public T this[int index]
+        {
+            get 
+            {
+                if (index >=0 && index < elements.Length)
+                    return elements[index];
+
+                return default;
+            }
+        }
+    }
+}
---
+++ Essential.Task 14.2/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Essential.Task_14._2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var names = new MyList<string>(["apple", "pear", "tomato"]);
+            var numbers = new MyList<int>([1, 45,67, 45,45,5,-9, 0]);
+
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+            foreach (int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+
+        }
+    }
+}
