48ad04c5cb949d3af881a181bafe68c3f8acb882
Volodymyr <Volodymyr@gmail.com>
2025-01-25 00:57:36 +0100

    Added 15.5

---
+++ Essential.Task 15.5/Calculator.cs
@@ -0,0 +1,73 @@
+﻿namespace Essential.Task_15._5
+{
+    delegate double Calculate(double a, double b);
+    static class Calculator
+    {
+        public static List<double> ResultsHistory { get; } = [];
+
+        public static double Add(double a, double b)
+        {
+           var result = a + b;
+           ResultsHistory.Add(result);
+           
+           return result;
+        }
+        public static double Sub(double a, double b)
+        {
+            var result = a - b;
+            ResultsHistory.Add(result);
+            
+            return result;
+        }
+        public static double Mul(double a, double b)
+        {
+           var result = a * b;
+           ResultsHistory.Add(result);
+            
+           return result;
+        }
+
+        public static double Div(double a, double b)  
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException("Critical error - Cannot divide by zero!");
+            }
+
+            var result = a / b;
+            ResultsHistory.Add(result);
+            
+            return result;  
+        }
+
+        public static double Calculate(double a, double b, Calculate method, out string? message)
+        {
+            message = null;
+
+            try
+            {
+                var result = method.Invoke(a, b);
+                ResultsHistory.Add(result);
+
+                return result;
+            }
+            catch (Exception e)
+            {
+                message = GetInnerMessage(e);
+
+                return -1;
+            }
+        }
+
+        private static string GetInnerMessage(Exception exception)
+        {
+            if (exception.InnerException != null)
+            {
+                return GetInnerMessage(exception.InnerException);  
+            }
+
+            return exception.Message;
+        }
+       
+    }
+}
---
+++ Essential.Task 15.5/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace Essential.Task_15._5
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Input a");
+            var answer = Console.ReadLine();
+            var aResult = Double.TryParse(answer, out double a);
+
+            Console.WriteLine("Input operation (+, -, /, *, Pif, ~)");
+            var sign = Console.ReadLine();
+
+            Console.WriteLine("Input b");
+            answer = Console.ReadLine();    
+            var bResult = Double.TryParse(answer, out double b);
+
+            if (aResult && bResult)
+            {
+                
+                var result = 0.0;
+                string? message = null;
+
+                switch (sign)
+                {
+                    case "+": result = Calculator.Add(a, b);
+                        break;
+                    case "-":
+                        result = Calculator.Sub(a, b);
+                       break;
+                    case "*":
+                        result = Calculator.Mul(a, b);
+                        break;
+                    case "/":
+                        try
+                        {
+                            result = Calculator.Div(a, b);
+                        }
+                        catch (DivideByZeroException ex) 
+                        {
+                           message = ex.Message;
+                        }
+                        break;
+                 case "Pif": result = Calculator.Calculate(a, b, (a, b) => Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2)), out message);
+                        break;
+                    case "~": result = Calculator.Calculate(a, b, (a, b) => (a + b) / 0, out message);
+                        break;
+                    default:
+                        Console.WriteLine("Not valid operation");
+                        break;
+                }
+
+                if (message is not null)
+                {
+                    Console.WriteLine($"Error has been occurred with such message '{message}'");
+                }
+                else
+                {
+                    Console.WriteLine($"Result {result:##.##}");
+                }
+            }
+        }
+    }
+}
